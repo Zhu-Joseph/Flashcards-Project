e445a6edf0a1126014a56bdc448ac11909f102f6
Joseph Zhu <josephzhu3@gmail.com>
2021-11-16 12:00:05 -0800

    Update React hook to speed up call

--- src/Deck/Deck.js
+++ src/Deck/Deck.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useLayoutEffect } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import Card from '../Card/Card';
 import { deleteDeck, readDeck } from '../utils/api';
@@ -8,7 +8,7 @@ export const Deck = () => {
   const history = useHistory();
   const [deck, setDeck] = useState(false);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     readDeck(deckId).then(setDeck);
   }, [deckId, deck]);
 
